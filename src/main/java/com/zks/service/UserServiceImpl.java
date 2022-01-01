c9fdb0b017e3cbc1282a31ef9bff261d5f69f2af
g15841693071 <g15841693071@163.com>
2022-01-01 21:48:02 +0800

    1、格式化

--- src/main/java/com/zks/service/UserServiceImpl.java
+++ src/main/java/com/zks/service/UserServiceImpl.java
@@ -7,25 +7,25 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.List;
 
 @Service(value = "userService")
 public class UserServiceImpl implements UserService {
+
     @Autowired
     private UserDao userDao;
 
     @Override
-    public MyResult login(User user){
-        List<User> list=new ArrayList<>();
-        list= userDao.login(user);
-        MyResult result=new MyResult();
-        if(list.size()!=0){
+    public MyResult login(User user) {
+        List<User> list = new ArrayList<>();
+        list = userDao.login(user);
+        MyResult result = new MyResult();
+        if (list.size() != 0) {
             result.setCode(0);
             result.setMsg("登陆成功！");
             result.setList(list);
             result.setObj(list.get(0));
-        }else {
+        } else {
             result.setCode(1);
             result.setMsg("登陆失败！");
         }
